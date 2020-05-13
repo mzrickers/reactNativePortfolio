7d2200ca47d23284f8b5df4d9843dd24eaf4ac01
Zach Rickers <mzrickers@gmail.com>
2020-05-13 07:22:21 -0600

    Add comment part 1

--- components/GameInfoComponent.js
+++ components/GameInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -28,15 +28,26 @@ function RenderGame(props) {
                 <Text style={{margin: 10}}>
                     {game.description}
                 </Text>
-                <Icon
-                    name={props.favorite ? 'heart' : 'heart-o'}
-                    type='font-awesome'
-                    color='#f50'
-                    raised
-                    reverse
-                    onPress={() => props.favorite ?
-                        console.log('Already set as a favorite') : props.markFavorite()}
-                />
+                <View style={styles.cardRow}>
+                    <Icon
+                        name={props.favorite ? 'heart' : 'heart-o'}
+                        type='font-awesome'
+                        color='#f50'
+                        raised
+                        reverse
+                        onPress={() => props.favorite ?
+                            console.log('Already set as a favorite') : props.markFavorite()}
+                    />
+                    <Icon
+                        style={styles.cardItem}
+                        name='pencil'
+                        type='font-awesome'
+                        color='green'
+                        raised
+                        reverse
+                        onPress={() => props.onShowModal()}
+                    />
+                </View>
             </Card>
         );
     }
@@ -71,6 +82,18 @@ function RenderComments({comments}) {
 
 class GameInfo extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showModal: false
+        }
+    }
+
+    toggleModal() {
+        this.setState({showModal: !this.state.showModal})
+    }
+
     markFavorite(gameId) {
         this.props.postFavorite(gameId);
     }
@@ -87,14 +110,49 @@ class GameInfo extends Component {
             <ScrollView>
                 <RenderGame game={game} 
                     favorite={this.props.favorites.includes(gameId)}
-                    markFavorite={() => this.markFavorite(gameId)}            
+                    markFavorite={() => this.markFavorite(gameId)} 
+                    onShowModal={() => this.toggleModal()}           
                 />
                 <RenderComments comments={comments} />
+                <Modal
+                    animationType={'slide'}
+                    transparent={false}
+                    visible={this.state.showModal}
+                    onRequestClose={() => this.toggleModal()}>
+                    <View style={styles.modal}>
+                        <View style={{margin: 10}}>
+                            <Button
+                                onPress={() => {
+                                    this.toggleModal();
+                                    // this.resetForm();
+                                }}
+                                color='#808080'
+                                title='Cancel'
+                            />
+                        </View>
+                    </View>
+                </Modal>
             </ScrollView>
         );
     } 
 }
 
-
+const styles = StyleSheet.create({
+    cardRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    cardItem: {
+        flex: 1,
+        margin: 10
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+    }
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(GameInfo);
