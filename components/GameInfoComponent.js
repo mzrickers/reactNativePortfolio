81a687fe869a56f20a6df592fa0b2315bc71c33b
Zach Rickers <mzrickers@gmail.com>
2020-05-14 11:02:01 -0600

    Added comment gesture

--- components/GameInfoComponent.js
+++ components/GameInfoComponent.js
@@ -28,7 +28,7 @@ function RenderGame(props) {
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
 
-    //const recognizeComment = ({dx}) => (dx > 200) ? true : false;
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -56,6 +56,8 @@ function RenderGame(props) {
                     ],
                     { cancelable: false }
                 );
+            } else if (recognizeComment(gestureState)) {
+                props.onShowModal();
             }
             return true;
         }
