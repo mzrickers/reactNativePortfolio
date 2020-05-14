7f6d12ec02fe8d4accbafc5dfa3530d1e03f52d7
Zach Rickers <mzrickers@gmail.com>
2020-05-14 10:11:38 -0600

    added redux persist

--- App.js
+++ App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
 
-const store = ConfigureStore();
+const { persistor, store } = ConfigureStore();
 
 import { YellowBox } from 'react-native';
 YellowBox.ignoreWarnings(['Warning: ...']) console.disableYellowBox = true;
 
 export default function App() {
   return (
-    <Provider store={store}>
-        <Main />
-    </Provider>
+      <Provider store={store}>
+          <PersistGate
+              loading={<Loading />}
+              persistor={persistor}>
+              <Main />
+          </PersistGate>
+      </Provider>
   );
 }
